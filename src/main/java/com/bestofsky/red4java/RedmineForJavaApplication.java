46099fb81a44afa30e705397daa2c6b4414bff0d
mins <heroms01@gmail.com>
2022-01-09 22:55:53 +0900

    프로젝트 초기화

---
+++ src/main/java/com/bestofsky/red4java/RedmineForJavaApplication.java
@@ -0,0 +1,13 @@
+package com.bestofsky.red4java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedmineForJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedmineForJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bestofsky/red4java/RedmineForJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestofsky.red4java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedmineForJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
